59e2da432b7eb4f7c1a44c65b4c16d50d5b86ab5
Victor Jonah <victorjonah@crenettechlabs.com>
2021-08-22 00:03:02 +0100

    ..

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const PROTO_PATH = './products.proto';
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const packageDefinistion = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
@@ -17,7 +17,7 @@ const productsProto = grpc.loadPackageDefinition(packageDefinistion);
 const server = new grpc.Server();
 
 // Using in-memory database
-const customers = [
+const products = [
   {
     id: 'a68b823c-7ca6-44bc-b721-fb4d5312cafc',
     name: 'Milo',
@@ -31,3 +31,46 @@ const customers = [
     description: 'Big size',
   },
 ];
+
+server.addService(productsProto.ProductService.service, {
+  getAll: (_, callback) => {
+    callback(null, { products });
+  },
+
+  get: (call, callback) => {
+    let product = products.find((n) => n.id == call.request.id);
+
+    if (products) callback(null, product);
+    else callback({ code: grpc.status.NOT_FOUND, details: 'Not found' });
+  },
+
+  insert: (call, callback) => {
+    let product = call.request;
+
+    product.id = uuidv4();
+    products.push(product);
+    callback(null, product);
+  },
+
+  update: (call, callback) => {
+    let existingProduct = products.find((n) => n.id == call.request.id);
+
+    if (existingProduct) {
+      existingProduct.name = call.request.name;
+      existingProduct.price = call.request.price;
+      existingProduct.description = call.request.description;
+    } else {
+      callback({ code: grpc.status.NOT_FOUND, details: 'Not found' });
+    }
+  },
+  remove: (call, callback) => {
+    let existingProductIndex = products.findIndex((n) => n.id == call.request.id);
+
+    if (existingProductIndex) {
+      products.splice(existingProductIndex, 1);
+      callback(null, {});
+    } else {
+      callback({ code: grpc.status.NOT_FOUND, details: 'Not found' });
+    }
+  },
+});
